a727ab309f28bd17542c3bab31923e11e53e168f
Maxim Kalinin <47250225+mkworkingman@users.noreply.github.com>
2020-10-12 16:06:21 +0400

    Register Backend

--- backend/gql/schema.py
+++ backend/gql/schema.py
@@ -1,6 +1,7 @@
 import graphene
 from items.models import Instrument, Siteuser
 from graphene_django.types import DjangoObjectType
+import re
 
 class SiteuserType(DjangoObjectType):
     class Meta:
@@ -38,6 +39,7 @@ class CreateUser(graphene.Mutation):
     username = graphene.String()
     email = graphene.String()
     password = graphene.String()
+    errors = graphene.List(graphene.String)
 
     class Arguments:
         username = graphene.String()
@@ -46,9 +48,52 @@ class CreateUser(graphene.Mutation):
         password2 = graphene.String()
 
     def mutate(self, info, username, email, password, password2):
+        errors = []
+        username = username.strip()
+        email = email.strip()
+        password = password.strip()
+        password2 = password2.strip()
 
-        if password == password2:
-            print("noice")
+        def isEnglish(s):
+            try:
+                s.encode(encoding='utf-8').decode('ascii')
+            except UnicodeDecodeError:
+                return False
+            else:
+                return True
+
+        password_test = isEnglish(password)
+
+        if len(username) == 0:
+            errors.append("Please enter your username.")
+        else:
+            if len(username) < 3 or len(username) > 22:
+                errors.append("Usename must have from 3 to 22 characters.")
+
+            if not re.match(r"^[A-Za-z0-9_-]*$", username):
+                errors.append("Username must contain only English letters, numbers, underscores and hyphens.")
+        
+        if len(email) == 0:
+            errors.append("Please enter your email.")
+        elif not re.match(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", email):
+                errors.append("Email is not valid.")
+
+        if len(password) == 0:
+            errors.append("Please enter your password.")
+        else:
+            if len(password) < 5:
+                errors.append("Password at least must have 6 symbols.")
+            if not password_test:
+                errors.append("Password must contain English letters.")
+            if re.match(r"\D*$", password):
+                errors.append("Password must have at least one number.")
+            if re.match(r"^[^A-Za-z]*$", password):
+                errors.append("Password must have at least one English letter.")
+        
+        if password != password2:
+            errors.append("Passwords don't match.")
+
+        print(errors)
 
         user = Siteuser(username=username, email=email, password=password)
         user.save()
@@ -59,7 +104,6 @@ class CreateUser(graphene.Mutation):
             email=user.email,
             password=user.password
         )
-
 
 class Mutation(graphene.ObjectType):
     create_user = CreateUser.Field()
