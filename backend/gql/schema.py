0267e25905897a993dcafd05e99774af20849c5e
Maxim Kalinin <47250225+mkworkingman@users.noreply.github.com>
2020-12-11 19:24:17 +0400

    Token exp and refresh

--- backend/gql/schema.py
+++ backend/gql/schema.py
@@ -5,7 +5,7 @@ from graphene_django.types import DjangoObjectType
 import re
 import bcrypt
 import jwt
-import datetime
+import time
 import json
 
 class AuthToken:
@@ -85,11 +85,22 @@ class LoginUser(graphene.Mutation):
 
         if valid:
             user = Siteuser.objects.get(**{login_type: login})
+
+            refresh_token = jwt.encode({
+                'id': user.id,
+                'username': user.username,
+                'email': user.email,
+                'exp': int(time.time()) + 31536000
+            }, 'mySuperSecretRefreshKey', algorithm='HS256').decode('utf-8')
+
+            print(refresh_token)
+
             auth_token = jwt.encode({
                 'id': user.id,
                 'username': user.username,
                 'email': user.email,
                 'added': user.added,
+                'exp': int(time.time()) + 300
             }, 'myTestKey!noiceone', algorithm='HS256').decode('utf-8')
             return AuthToken(auth_token)
         return ValidationErrors(errors)
@@ -218,6 +229,7 @@ class ChangeAdded(graphene.Mutation):
                 'username': user.username,
                 'email': user.email,
                 'added': added_json,
+                'exp': int(time.time()) + 300
             }, 'myTestKey!noiceone', algorithm='HS256').decode('utf-8')
             return AuthToken(auth_token)
 
